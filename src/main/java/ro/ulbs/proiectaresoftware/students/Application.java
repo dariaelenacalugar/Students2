54f874762e30d536c6fe436c99b752978fb27afb
Daria Calugar <dariaelena.calugar@ulbsibiu.ro>
2026-05-25 23:30:32 +0300

    Lab3_students

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,6 +1,11 @@
 package ro.ulbs.proiectaresoftware.students;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Application {
@@ -54,6 +59,53 @@ public class Application {
                 + " (" + cautat2.getFormatieDeStudiu() + ") este in lista: "
                 + existaStudent(students, cautat2));
 
+        students.sort((a, b) -> a.getNume().compareTo(b.getNume()));
+
+        try {
+            List<String> liniiOut = new ArrayList<>();
+            for (Student s : students) {
+                liniiOut.add(s.getNumarMatricol() + "," + s.getNume() + "," + s.getPrenume() + "," + s.getFormatieDeStudiu());
+            }
+            Files.write(Paths.get("studenti_out.txt"), liniiOut);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ;
+        for (Student s : students) {
+            System.out.println(s);
+        }
+        //listStudenti.sort((s2,s3)->s2.getFormatieDeStudiu().compareTo(s3.getFormatieDeStudiu()));
+        students.sort((s6, s7) -> s6.getNume().compareTo(s7.getNume()));
+
+        try {
+            List<String> liniiOut = new ArrayList<>();
+            for (Student s : students) {
+                liniiOut.add(s.getNumarMatricol() + "," + s.getNume() + "," + s.getPrenume() + "," + s.getFormatieDeStudiu());
+            }
+            Files.write(Paths.get("studenti_out.txt"), liniiOut);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Tema lab3
+        Collections.sort(students, new Comparator<Student>() {
+            @Override
+            public int compare(Student s1, Student s2) {
+                int res = s1.getFormatieDeStudiu().compareTo(s2.getFormatieDeStudiu());
+                if (res == 0) {
+                    res = s1.getNume().compareTo(s2.getNume());
+                }
+                return res;
+            }
+        });
+        List<String> liniiSort = new ArrayList<>();
+        for (Student s : students) {
+            liniiSort.add(s.getNumarMatricol() + "," + s.getNume() + "," + s.getPrenume() + "," + s.getFormatieDeStudiu());
+        }
+        try {
+            Files.write(Path.of("studenti_out_sorted.txt"), liniiSort);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
