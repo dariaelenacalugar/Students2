3b6b593a94cd9392d9a957a4b7bf9a0b62c3150a
Daria Calugar <dariaelena.calugar@ulbsibiu.ro>
2026-05-25 23:05:50 +0300

    Lab2_students

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,7 +1,21 @@
 package ro.ulbs.proiectaresoftware.students;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static boolean existaStudent(List<Student> lista, Student student) {
+        for (Student s : lista) {
+            if (s.getPrenume().equals(student.getPrenume()) &&
+                    s.getNume().equals(student.getNume()) &&
+                    s.getFormatieDeStudiu().equals(student.getFormatieDeStudiu())) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
+        //Lab1
         Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
         Student s2 = new Student(112, "Maria", "Oprea", "TI21/1");
         Student s3 = new Student(120, "Alis", "Popa", "TI21/2");
@@ -12,6 +26,33 @@ public class Application {
         System.out.println(s3);
         System.out.println(s4);
         System.out.println(s5);
+
+        //Lab2
+        //a)
+        List<Student> students=new ArrayList<>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+        students.add(s5);
+
+        System.out.println("Lista studenti a) :");
+        for (Student s : students) {
+            System.out.println(s);
+        }
+        // b)
+        Student cautat1 = new Student(120, "Alis", "Popa", "TI21/2");
+        System.out.println("Verificare b)");
+        System.out.println("Studentul " + cautat1.getPrenume() + " " + cautat1.getNume()
+                + " (" + cautat1.getFormatieDeStudiu() + ") este in lista: "
+                + existaStudent(students, cautat1));
+
+        // c) Verificare
+        Student cautat2 = new Student(112, "Maria", "Popa", "TI21/1");
+        System.out.println("Verificare c)");
+        System.out.println("Studentul " + cautat2.getPrenume() + " " + cautat2.getNume()
+                + " (" + cautat2.getFormatieDeStudiu() + ") este in lista: "
+                + existaStudent(students, cautat2));
 
     }
 
